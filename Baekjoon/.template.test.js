934b316a3d264c6029601e33b35a7c2b992e2dd8
vinitus <tlsdnrng@gmail.com>
2023-11-08 10:49:15 +0900

    test case 문자열 만들기

--- Baekjoon/.template.test.js
+++ Baekjoon/.template.test.js
@@ -1,12 +1,29 @@
 const main = require('./.template');
 
-function* counter() {
+function* makeTestCaseNumGenerator() {
   let cnt = 1;
   while (cnt <= 100) {
     yield cnt++;
   }
 }
 
-test(`번호: 문제이름 case ${counter().next().value}`, () => {
+const makeTestCaseNum = makeTestCaseNumGenerator();
+
+function getTestCaseString() {
+  const fileDirArr = __filename.split('\\');
+  const fileFullname = fileDirArr[fileDirArr.length - 1];
+  const filename = fileFullname.split('.test.js')[0];
+  const [problemNum, problemName] = filename.split('_');
+
+  return `${problemNum}: ${problemName} case`;
+}
+
+const testCaseString = getTestCaseString();
+
+test(`${testCaseString} ${makeTestCaseNum.next().value}`, () => {
+  expect(main()).toBe(undefined);
+});
+
+test(`${testCaseString} ${makeTestCaseNum.next().value}`, () => {
   expect(main()).toBe(undefined);
 });
