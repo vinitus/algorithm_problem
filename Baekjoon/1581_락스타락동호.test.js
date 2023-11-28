b681e558719474a56e061362d76649d86bef24ea
vinitus <tlsdnrng@gmail.com>
2023-11-28 21:57:27 +0900

    1581

---
+++ Baekjoon/1581_락스타락동호.test.js
@@ -0,0 +1,33 @@
+const main = require('./1581_락스타락동호');
+
+function* makeTestCaseNumGenerator() {
+  let cnt = 1;
+  while (cnt <= 100) {
+    yield cnt++;
+  }
+}
+
+const makeTestCaseNum = makeTestCaseNumGenerator();
+
+function getTestCaseString() {
+  const fileDirArr = __filename.split('\\');
+  const fileFullname = fileDirArr[fileDirArr.length - 1];
+  const filename = fileFullname.split('.test.js')[0];
+  const [problemNum, problemName] = filename.split('_');
+
+  return `${problemNum}: ${problemName} case`;
+}
+
+const testCaseString = getTestCaseString();
+
+const makeTestInstance = (idx, result) => {
+  return test(`${testCaseString} ${makeTestCaseNum.next().value}`, () => {
+    expect(main(idx)).toBe(result);
+  });
+};
+
+makeTestInstance(`1 2 1 1`, 5);
+makeTestInstance(`100 0 0 200`, 100);
+makeTestInstance(`0 0 20 200`, 201);
+makeTestInstance(`192 279 971 249`, 999);
+// makeTestInstance(undefined, undefined);
