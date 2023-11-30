a33091b72438af63302343bb80574b3802b410be
vinitus <tlsdnrng@gmail.com>
2023-11-30 21:45:07 +0900

    2758

---
+++ Baekjoon/2758_로또.test.js
@@ -0,0 +1,41 @@
+const main = require('./2758_로또');
+
+function* makeTestCaseNumGenerator() {
+  let cnt = 1;
+  while (cnt <= 100) {
+    yield cnt++;
+  }
+}
+
+const makeTestCaseNum = makeTestCaseNumGenerator();
+
+function getTestCaseString() {
+  const fileDirArr = __filename.split('\\');
+  const fileFullname = fileDirArr[fileDirArr.length - 1];
+  const filename = fileFullname.split('.test.js')[0];
+  const [problemNum, problemName] = filename.split('_');
+
+  return `${problemNum}: ${problemName} case`;
+}
+
+const testCaseString = getTestCaseString();
+
+const makeTestInstance = (idx, result) => {
+  return test(`${testCaseString} ${makeTestCaseNum.next().value}`, () => {
+    expect(main(idx)).toBe(result);
+  });
+};
+
+makeTestInstance(
+  `1
+4 10`,
+  '4'
+);
+makeTestInstance(
+  `2
+3 4
+2 2`,
+  `1
+1`
+);
+// makeTestInstance(undefined, undefined);
